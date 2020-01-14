18803ef58b4907d30fb35ee64b93d1d86e3da5be
jake <jakecollins1014@gmail.com>
2020-01-13 22:00:22 -0500

    if-elses added

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -4,9 +4,29 @@ namespace Lab_3
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your number?");
+            string input = Console.ReadLine();
+            int guess = int.Parse(input);
+
+            if (guess % 2 != 0){
+                Console.WriteLine(guess + " Odd.");
+            }
+            else if (guess % 2 == 0 && guess >= 2 && guess <= 25)
+            {
+                Console.WriteLine("Even and less than 25.");
+            }
+            else if (guess % 2 == 0 && guess >= 26 && guess <= 60)
+            {
+                Console.WriteLine("Even.");
+            }
+            else if (guess % 2 == 0 && guess > 60)
+            {
+                Console.WriteLine(guess + " Even.");
+            }
         }
     }
 }
