d5f99020ceeca40f973f07d6afec52cb8ec46cb0
jake <jakecollins1014@gmail.com>
2020-01-15 21:38:21 -0500

    added comments

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -6,15 +6,18 @@ namespace Lab_3
     {
         static void Main(string[] args)
         {
+            //initial user input and variable declaration
             Console.WriteLine("Hello! What is your name?");
             string name = Console.ReadLine();
             int guess;
             string output;
 
+            //user input for numbers and converting from a string to int
             Console.WriteLine("What is your number? Please choose between 1 and 100.");
             string input = Console.ReadLine();
             guess = int.Parse(input);
 
+            //input validation for number
             while (guess < 1 || guess > 100)
             {
                 Console.WriteLine("Please input a valid number. Please choose between 1 and 100.");
@@ -22,6 +25,7 @@ namespace Lab_3
                 guess = int.Parse(input);
             }
 
+            //checking the number against test conditions
             if (guess % 2 == 0 && guess >= 2 && guess <= 25)
             {
                 output = "even and less than 25.";
@@ -39,8 +43,11 @@ namespace Lab_3
                 output = guess.ToString() + " and odd.";
             }
 
+            //final output for number
             Console.WriteLine(name + ", Your number is " + output);
-            Environment.Exit(0);
+
+            //ending program successfully
+            Environment.Exit(1);
         }
     }
 }
