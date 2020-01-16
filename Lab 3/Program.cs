f46720e3ed71b7728ce58458cb49fbbb01a1cba0
jake <jakecollins1014@gmail.com>
2020-01-15 20:14:11 -0500

    refactored input validation & if/else tree

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -4,8 +4,6 @@ namespace Lab_3
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! What is your name?");
@@ -13,31 +11,38 @@ namespace Lab_3
             int guess = 0;
             string output = "";
 
+            Console.WriteLine("What is your number? Please choose between 1 and 100.");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            output = "";
+
             while (guess < 1 || guess > 100)
             {
-                Console.WriteLine("What is your number? Please choose between 1 and 100");
-                string input = Console.ReadLine();
+                Console.WriteLine("Please input a valid number. Please choose between 1 and 100.");
+                input = Console.ReadLine();
                 guess = int.Parse(input);
                 output = "";
             }
 
-            if (guess % 2 != 0){
-                output = guess.ToString() + " odd.";
-            }
-            else if (guess % 2 == 0 && guess >= 2 && guess <= 25)
+            if (guess % 2 == 0 && guess >= 2 && guess <= 25)
             {
                 output = "even and less than 25.";
             }
             else if (guess % 2 == 0 && guess >= 26 && guess <= 60)
             {
-                output = guess.ToString() + " and even.";
+                output = "even.";
             }
             else if (guess % 2 == 0 && guess > 60)
             {
-                output = "even.";
+                output = guess.ToString() + " and even.";
+            }
+            else
+            {
+                output = guess.ToString() + " and odd.";
             }
 
             Console.WriteLine(name + ", Your number is " + output);
+            Environment.Exit(0);
         }
     }
 }
